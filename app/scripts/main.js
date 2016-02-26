2328f17393fa227c8a6e093259c825f636f3383d
Asher <asher@esc3051-dell-n-provart.utoronto.ca>
2016-02-26 14:02:53 -0500

    Initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,8 @@
+/* globals window, AIP, $ */
+  'use strict';
+  window.addEventListener('Agave::ready', function() {
+    if(console){
+        console.log('Agave has been initialized, do something');
+          
+    }
+  });
